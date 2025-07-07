744e1faf56dd1d184214d6f57cb02369b2527ca5
Eman Tariq <emantariq.1004@gmail.com>
2025-07-07 21:06:49 +0500

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const celsiusEl = document.getElementsById("celsius");
+const fahrenheitEl = document.getElementsById("celsius");
+const kelvinEl = document.getElementsById("kelvin");
+
+function computeTemp(event) {
+    const currentValue = +event.target.value;
+}
