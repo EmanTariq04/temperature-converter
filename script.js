d9c2da18b57504b20323a539caf87ca9a2ae5ef9
Eman Tariq <emantariq.1004@gmail.com>
2025-07-08 12:05:54 +0500

    commit

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const celsiusEl = document.getElementsById("celsius");
-const fahrenheitEl = document.getElementsById("celsius");
-const kelvinEl = document.getElementsById("kelvin");
+const celsiusEl = document.getElementById("celsius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
 
 function computeTemp(event) {
-  const currentValue = +event.target.value;
+  const currentValue = +event.target.value; //.value = the number typed in (as a string). + in front = converts it to a number.
 
-  switch (event.target.name) {
+  switch (
+    event.target.name //This checks which input box the user is typing in:
+  ) {
+    //If the box has the name "celsius" → run the Celsius formula and so on
     case "celsius":
       kelvinEl.value = (currentValue + 273.32).toFixed(2);
       fahrenheitEl.value = (currentValue + 1.8 + 32).toFixed(2);
       break;
     case "fahrenheit":
-      celsiusEl.value = ((currentValue * 32) / 1.8).toFixed(2);
-      fahrenheitEl.value = (currentValue - 32 / 1.8 + 273.32).toFixed(2);
+      celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
+      kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
       break;
+
     case "kelvin":
       celsiusEl.value = (currentValue - 273.32).toFixed(2);
       fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
